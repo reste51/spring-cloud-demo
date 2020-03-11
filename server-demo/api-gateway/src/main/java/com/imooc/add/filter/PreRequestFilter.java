e8b7404f7f6a6ba5af3c9e8bf1fb8999d510dc85
reste51 <61609268+reste51@users.noreply.github.com>
2020-03-11 21:34:18 +0800

    创建 add-common 模块来 处理： 统一响应数据

--- server-demo/api-gateway/src/main/java/com/imooc/add/filter/PreRequestFilter.java
+++ server-demo/api-gateway/src/main/java/com/imooc/add/filter/PreRequestFilter.java
@@ -1,13 +1,15 @@
 package com.imooc.add.filter;
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
 import org.springframework.stereotype.Component;
 
 /**
  * 前置过滤器
- * 1. 记录请求前的 时间
+ *  记录请求前的 时间
  * @ClassName PreRequestFilter
  * @Description TODO
  * @Author 50204
@@ -19,21 +21,38 @@ import org.springframework.stereotype.Component;
 public class PreRequestFilter  extends ZuulFilter {
     @Override
     public String filterType() {
-        return  "";
+        return FilterConstants.PRE_TYPE;
     }
 
+    /**
+     * 值越少 则 调用的优先级越高。
+     * @return
+     */
     @Override
     public int filterOrder() {
         return 0;
     }
 
+    /**
+     * 执行该方法
+     * @return
+     */
     @Override
     public boolean shouldFilter() {
-        return false;
+        return true;
     }
 
+    /**
+     * 请求时 运行的方法
+     * @return
+     * @throws ZuulException
+     */
     @Override
     public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        // 设置 请求的开始时间
+        ctx.set("startTime",System.currentTimeMillis());
+
         return null;
     }
 }

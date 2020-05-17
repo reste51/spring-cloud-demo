a7a59123c1a50f07dc806467d53f36a4d601aa76
reste51 <502042437@qq.com>
2020-05-17 19:09:14 +0800

    Kafka 学习

--- server-demo/kafka-study/src/main/java/com/imooc/kafka/study/MyProducer.java
+++ server-demo/kafka-study/src/main/java/com/imooc/kafka/study/MyProducer.java
@@ -1,9 +1,14 @@
 package com.imooc.kafka.study;
 
+import com.imooc.kafka.study.util.KafkaPropUtil;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * kafka的生产者
@@ -31,13 +36,72 @@ public class MyProducer {
 
     // 1.发送消息_ 只发送不管结果
     private static void  sendMessageForgetResult(){
-        ProducerRecord<String,String> record = new ProducerRecord<String, String>("test3","name","ForgetResult");
+        ProducerRecord<String,String> record = new ProducerRecord<String, String>("cs111","name","ForgetResult");
         producer.send(record);
         producer.close();
     }
-
-    public static void main(String[] args) {
-        sendMessageForgetResult();
+    /*
+        2. 非阻塞方式 发送
+        注: 阻塞的时， 打印数字是 有顺序的， 而异步则是  无顺序的
+      */
+    private static void  sendMsgWithNonblocking(){
+        Producer<String, String> producer = new KafkaProducer<>(KafkaPropUtil.getProperties());
+        for(int i = 0; i < 100; i++) {
+            //异步，添加消息到缓存区，等待到一定程度把这些消息一起发送到集群
+            producer.send(new ProducerRecord<String, String>("cs111",  Integer.toString(i)));
+        }
+        producer.close();  //关闭生产者，不关闭为造成还没发送过去的消息发生泄漏
     }
+
+    /*
+        3. 有返回值的阻塞
+        接收了返回值他还是非阻塞的，Future调用了get()方法是就成阻塞的了，  直到相关请求完成返回RecordMetadata 或者抛出异常
+        可以自己试试，当把第二行注释起来很快，放开就会速度慢下来
+        注: 阻塞的时， 打印数字是 有顺序的， 而异步则是  无顺序的
+     */
+    public static  void  sendMsgWithResult() throws ExecutionException, InterruptedException {
+        Producer<String, String> producer = new KafkaProducer<>(KafkaPropUtil.getProperties());
+
+        for(int i = 0; i < 100; i++) {
+            //异步，添加消息到缓存区，等待到一定程度把这些消息一起发送到集群
+            Future<RecordMetadata> frm = producer.send(new ProducerRecord<String, String>("cs111",  Integer.toString(i)+"：is Blocking!"));
+            RecordMetadata rm = frm.get();
+            System.out.println(String.format("阻塞信息为：topic: %s, 分区为: %s",rm.topic(),rm.partition()));
+        }
+        producer.close();
+    }
+    /*
+     4. 接收回调函数的非阻塞
+      如果你想提高性能也就是非阻塞，但是你想知道传过去了吗，也就是成功了吗
+      如果你想通过判断 Future<RecordMetadata> 是否为 null ，那你肯定不对，因为不管成不成功，Future<RecordMetadata> 都不为null
+     */
+    public static void  sendMsgWithCallback() throws ExecutionException, InterruptedException {
+        Producer<String, String> producer = new KafkaProducer<>(KafkaPropUtil.getProperties());
+
+        for(int i = 0; i < 100; i++) {
+            //异步，添加消息到缓存区，等待到一定程度把这些消息一起发送到集群
+            ProducerRecord record = new ProducerRecord<String, String>("cs111",  Integer.toString(i)+"：is Blocking!");
+            Future<RecordMetadata> frm = producer.send(record,(recordMetadata, e) -> {
+                if(e!=null){
+                    System.out.println(" 调用出错！ 请稍后再试");
+                }else {
+                    System.out.println("调用成功！");
+                }
+            });
+            // 注释为 异步, 开启为  同步
+            RecordMetadata rm = frm.get();
+            System.out.println(String.format("阻塞信息为：topic: %s, 分区为: %s",rm.topic(),rm.partition()));
+        }
+        producer.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+//        sendMessageForgetResult();
+//        sendMsgWithNonblocking();
+//        sendMsgWithResult();
+
+        sendMsgWithCallback();
+    }
+
 
 }
---
+++ server-demo/kafka-study/src/main/java/com/imooc/kafka/study/util/KafkaPropUtil.java
@@ -0,0 +1,54 @@
+package com.imooc.kafka.study.util;
+
+import java.util.Properties;
+
+/**
+ * kafka的配置
+ * @ClassName KafkaUtil
+ * @Description TODO
+ * @Author DELL
+ * @Date 2020/5/17 12:25
+ * @Version 1.0
+ **/
+public class KafkaPropUtil {
+
+    /**
+     * 获取 kafka 的通用配置
+     * @return
+     */
+    public static Properties getCommonProp() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "192.168.240.128:9092");
+        props.put("acks", "all");       //判定是否成功发送，“all”会阻塞，性能低但可靠
+        props.put("retries", 0);        //失败重试次数
+        props.put("batch.size", 16384);//缓存区的大小，每个"活跃"的分区有一个缓存区
+        /*
+            消息延迟时间，单位毫秒，小于这个时间的消息组成批一次请求就发送过去，
+            在高负载情况下时间间隔太近的也会组成批发送
+            不再高负载下可以时间大点，以时间换有效的请求
+        */
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);//缓存的总量，大于这个数就会阻塞
+        //key和value 序列化 转成字节
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        return props;
+    }
+
+    /**
+     *  获取偏移量的kafka
+     * @return
+     */
+    public static Properties getProperties() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "192.168.240.128:9092");
+        props.put("group.id", "test");   //消费者组 ，不能不写，这个不一定和9092的消费者的group.id对应，随便写的
+        props.put("enable.auto.commit", "true"); //自动提交偏移量为true，不自动提交则为false
+        props.put("auto.commit.interval.ms", "1000");//自动提交的频率，一秒一次
+        props.put("session.timeout.ms", "30000");//停止心跳时间超过30s，就认为有故障
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        return props;
+    }
+
+}
